5237ed636c087df56508d42f7d593d6106150fb5
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2023-05-13 15:09:41 +0300

    fetch pokemon

--- dist/cors.5da44700.js
+++ dist/cors.5da44700.js
@@ -117,44 +117,66 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"js/01-promise-api.js":[function(require,module,exports) {
-var promise = new Promise(function (resolve, reject) {
-  var canFulfill = Math.random() > 0.5;
-  setTimeout(function () {
-    if (canFulfill) {
-      resolve('Промис выполнился успешно, с результатом (исполнен, fulfilled)');
+})({"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
+  }
+  return bundleURL;
+}
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+    if (matches) {
+      return getBaseURL(matches[0]);
     }
-    reject('Промис выполнился с ошибкой (отклонён, rejected)');
-  }, 2000);
-});
-
-// promise.then(onFulfilled, onRejected);
-
-function onFulfilled(result) {
-  console.log("onFulfilled -> onFulfilled:");
-  console.log(result);
+  }
+  return '/';
 }
-;
-function onRejected(error) {
-  console.log("onRejected -> onRejected:");
-  console.log(error);
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)?\/[^/]+(?:\?.*)?$/, '$1') + '/';
 }
-;
-promise.then(onFulfilled).then(function (x) {
-  console.log("x :", x);
-  return 10;
-}).then(function (y) {
-  console.log("y:", y);
-  throw new Error('ошибка в третьем then');
-  return 5;
-}).then(function (z) {
-  console.log("z:", z);
-}).catch(function (error) {
-  return console.log(error);
-}).finally(function () {
-  return console.log('Я буду выполнен в любом случае');
-});
-},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+function updateLink(link) {
+  var newLink = link.cloneNode();
+  newLink.onload = function () {
+    link.remove();
+  };
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+var cssTimeout = null;
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+    cssTimeout = null;
+  }, 50);
+}
+module.exports = reloadCSS;
+},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"css/common.css":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"js/cors.js":[function(require,module,exports) {
+"use strict";
+
+require("../css/common.css");
+},{"../css/common.css":"css/common.css"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -323,5 +345,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/01-promise-api.js"], null)
-//# sourceMappingURL=/01-promise-api.1ee98991.js.map
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/cors.js"], null)
+//# sourceMappingURL=/cors.5da44700.js.map
--- src/js/fetch-api.js
+++ src/js/fetch-api.js
@@ -1,1 +1,13 @@
 import '../css/common.css';
+import pokemopCardTpl from '../templates/pokemon-card.hbs';
+
+fetch('https://pokeapi.co/api/v2/pokemon/2').then(response => {
+     return response.json();     
+}).then(pokemon => {
+     console.log("fetch  pokemon:", pokemon);
+     const pokemonMarkup = pokemopCardTpl(pokemon);
+     console.log("fetch  pokemonMarkup:", pokemonMarkup)
+     
+}).catch(error => {
+     console.log("fetch  error:", error)    
+});
